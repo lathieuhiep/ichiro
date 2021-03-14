cf8db93688997cf029b09d1e97bc19f43382b0fa
KhacDiepKMA <khacdiepkma90@gmail.com>
2021-03-14 10:03:57 +0700

    update code

--- template-parts/footer/inc-copyright.php
+++ template-parts/footer/inc-copyright.php
@@ -8,38 +8,6 @@ $ichiro_copyright = $ichiro_options ['ichiro_footer_copyright_editor'] == '' ? '
 
 <div class="site-footer__copyright">
     <div class="container">
-        <div class="site-copyright-menu d-flex align-items-center">
-            <div class="site-copyright">
-                <?php echo wp_kses_post( $ichiro_copyright ); ?>
-            </div>
-
-            <div class="site-footer__menu">
-                <nav>
-                    <?php
-
-                    if ( has_nav_menu( 'footer-menu' ) ) :
-
-                        wp_nav_menu( array(
-                            'theme_location'    => 'footer-menu',
-                            'menu_class'        => 'menu-footer',
-                            'container'         =>  false,
-                        ));
-
-                    else:
-
-                    ?>
-
-                        <ul class="main-menu">
-                            <li>
-                                <a href="<?php echo get_admin_url().'/nav-menus.php'; ?>">
-                                    <?php esc_html_e( 'ADD TO MENU','ichiro' ); ?>
-                                </a>
-                            </li>
-                        </ul>
-
-                    <?php endif;?>
-                </nav>
-            </div>
-        </div>
+	    <?php echo wp_kses_post( $ichiro_copyright ); ?>
     </div>
 </div>
